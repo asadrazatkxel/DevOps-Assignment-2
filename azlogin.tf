abba5a40e1d8b3c2f11e08ab10707a528508fe79
asadrazatkxel <asad.raza@tkxel.io>
2024-09-30 10:41:06 +0000

    terraform is dome

--- azlogin.tf
+++ azlogin.tf
@@ -12,7 +12,8 @@ terraform {
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-  
+
   # Use the subscription_id from `az account show`
-  subscription_id = "a2b28c85-1948-4263-90ca-bade2bac4df4"  
+  subscription_id                 = "a2b28c85-1948-4263-90ca-bade2bac4df4"
+  resource_provider_registrations = "none"
 }
--- linux-vm.tf
+++ linux-vm.tf
@@ -1,39 +1,51 @@
 resource "azurerm_resource_group" "myazurelinuxvm" {
-  name     = "myazurelinuxvm-resources"
-  location = "East US"
+  name     = "kml_rg_main-38562b46e13443fa"
+  location = "West US"
 }
 
 resource "azurerm_virtual_network" "myazurelinuxvm" {
   name                = "myazurelinuxvm-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.myazurelinuxvm.location
-  resource_group_name = azurerm_resource_group.myazurelinuxvm.name
+  resource_group_name = "kml_rg_main-38562b46e13443fa"
 }
 
 resource "azurerm_subnet" "myazurelinuxvm" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.myazurelinuxvm.name
+  resource_group_name  = "kml_rg_main-38562b46e13443fa"
   virtual_network_name = azurerm_virtual_network.myazurelinuxvm.name
   address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "myazurelinuxvm" {
+  name                = "acceptanceTestPublicIp1"
+  resource_group_name = "kml_rg_main-38562b46e13443fa"
+  location            = azurerm_resource_group.myazurelinuxvm.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "Dev-1"
+  }
 }
 
 resource "azurerm_network_interface" "myazurelinuxvm" {
   name                = "myazurelinuxvm-nic"
   location            = azurerm_resource_group.myazurelinuxvm.location
-  resource_group_name = azurerm_resource_group.myazurelinuxvm.name
+  resource_group_name = "kml_rg_main-38562b46e13443fa"
 
   ip_configuration {
     name                          = "internal"
     subnet_id                     = azurerm_subnet.myazurelinuxvm.id
     private_ip_address_allocation = "Dynamic"
+    public_ip_address_id  = azurerm_public_ip.myazurelinuxvm.id
   }
 }
 
 resource "azurerm_linux_virtual_machine" "myazurelinuxvm" {
   name                = "myazurelinuxvm-machine"
-  resource_group_name = azurerm_resource_group.myazurelinuxvm.name
+  resource_group_name = "kml_rg_main-38562b46e13443fa"
   location            = azurerm_resource_group.myazurelinuxvm.location
-  size                = "Standard_F2"
+  size                = "Standard_B2s"
   admin_username      = "adminuser"
   network_interface_ids = [
     azurerm_network_interface.myazurelinuxvm.id,
